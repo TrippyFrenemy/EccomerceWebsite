abf3d8a82941de71f8a74a98b309404752d06114
Avruschenko Dmytro <trippyfren@gmail.com>
2023-03-25 09:31:05 +0200

    Add product page that will have more details

--- EccomercialWebsite/settings.py
+++ EccomercialWebsite/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "EccomercialWebsite/static/"
+    BASE_DIR / "EccomercialWebsite/static/",
+    #BASE_DIR / "static/",
 ]
 
 STATIC_ROOT = BASE_DIR / "static"
--- shop/views.py
+++ shop/views.py
@@ -9,3 +9,6 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+
+def product(request):
+    return render(request, "product.html")

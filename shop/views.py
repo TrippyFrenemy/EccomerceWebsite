e3e85d6dcbd04a1c1ff5fd836b219c77e6d09c91
Avruschenko Dmytro <trippyfren@gmail.com>
2023-03-31 15:27:18 +0300

    Add func to cart page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Product, Cart, CartItem
 
 
 # Create your views here.
@@ -33,5 +34,43 @@ def product(request, category_slug=None, product_slug=None):
     return render(request, "product.html", dict)
 
 
-def cart(request):
-    return render(request, "cart.html")
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+        cart.save()
+    try:
+        cart_item = CartItem.objects.get(product= product, cart= cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(product= product, cart= cart, quantity=1)
+        cart_item.save()
+
+    return redirect("cart_detail")
+
+
+def cart_detail(request, total=0, counter=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
+        for item in cart_items:
+            total += (item.product.price * item.quantity)
+            counter += item.quantity
+    except ObjectDoesNotExist:
+        pass
+    dict = dict(
+        cart_items=cart_items,
+        total=total,
+        counter=counter
+    )
+    return render(request, "cart.html", dict)

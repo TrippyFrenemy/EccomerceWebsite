81edc46a69072820b36c671247c5e42a5c59cc19
Avruschenko Dmytro <trippyfren@gmail.com>
2023-03-31 15:53:01 +0300

    Add cart functionality, cart page with unfull functionality

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("cart/", views.cart, name="cart"),
     path("category/<slug:category_slug>", views.home, name="products_by_category"),
     path("category/<slug:category_slug>/<slug:product_slug>", views.product, name="product_detail"),
     path("about/", views.about, name="about"),
+    path("cart/", views.cart_detail, name="cart_detail"),
+    path("cart/add/<int:product_id>", views.add_cart, name="add_cart"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -68,9 +68,8 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
             counter += item.quantity
     except ObjectDoesNotExist:
         pass
-    dict = dict(
+    return render(request, "cart.html", dict(
         cart_items=cart_items,
         total=total,
         counter=counter
-    )
-    return render(request, "cart.html", dict)
+    ))

a9f5e2500f7f6e099238a8d40d1160acbdd06393
Avruschenko Dmytro <trippyfren@gmail.com>
2023-04-01 12:15:52 +0300

    Create functional of minus and removing products

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("cart/", views.cart_detail, name="cart_detail"),
     path("cart/add/<int:product_id>", views.add_cart, name="add_cart"),
+    path("cart/remove/<int:product_id>", views.cart_remove, name="cart_remove"),
+    path("cart/delete/<int:product_id>", views.cart_delete, name="cart_delete"),
 ]
